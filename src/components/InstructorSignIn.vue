69a2b93dab17742d22462b5e899aad42700a3b76
John Pham <johnphammail@gmail.com>
2018-01-03 13:00:57 -0800

    adding instructor sign in and out view

--- src/components/InstructorSignIn.vue
+++ src/components/InstructorSignIn.vue
@@ -1,6 +1,26 @@
 <template>
-  <arc-card class="text-center">
-    <h1>Instructor Sign In</h1>
+  <arc-card class="text-center" type="success">
+    <h1 slot="header">Instructor Sign In</h1>
+
+    <div class="mb-5 mt-5">
+      <div class="card-block">
+        <button class="btn btn-primary btn-pill btn-lg d-flex ml-auto mr-auto" @click="logIn">Instructor Sign In</button>
+
+        <hr />
+
+        <div class="alert alert-warning" role="alert">
+          Only those who authorized are allowed to create an account. Contact your supervisor for the access code.
+        </div>
+        <label for="accessCode">Access Code</label>
+        <input id="accessCode" type="text" class="form-control ml-auto mr-auto mb-3" v-model="accessCode">
+        <button 
+          class="btn btn-info btn-pill btn-lg d-flex ml-auto mr-auto"
+          :disabled="!validCode"
+          :class="{ disabled : !validCode}"
+          @click="createAccount"
+        >Create an Account</button>
+      </div>
+    </div>
   </arc-card>
 </template>
 
@@ -10,10 +30,34 @@ import Card from './Card'
 export default {
     components: {
         arcCard: Card
+    },
+    data: function() {
+        return {
+            accessCode: ''
+        }
+    },
+    computed: {
+        validCode() {
+            return this.accessCode === 'SIReg'
+        }
+    },
+    methods: {
+        createAccount() {
+            console.log('Create account')
+        },
+        logIn() {
+            console.log('Log in')
+            this.$router.push('/classes')
+        }
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+.form-control {
+    width: 40%;
+}
+.disabled {
+    cursor: not-allowed;
+}
 </style>

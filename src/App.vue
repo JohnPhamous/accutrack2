bfc4cffa10cc62388a62242734d2d25c4b81b6f3
John Pham <johnphammail@gmail.com>
2019-01-07 09:59:36 -0800

    adding banner to point to new version

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div class="arc-landing-page--1">
     <div class="welcome d-flex justify-content-center flex-column">
       <div class="container navbar-container">
+        <arc-banner/>
         <arc-navbar/>
       </div>
       <transition name="slide" mode="out-in">
@@ -13,65 +14,67 @@
 </template>
 
 <script>
-import Navbar from './components/NavBar'
-import Footer from './components/Footer'
+import Navbar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Banner from "./components/Banner/Banner";
 
-import 'font-awesome/css/font-awesome.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "font-awesome/css/font-awesome.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import 'jquery'
 
 export default {
-    components: {
-        arcNavbar: Navbar,
-        arcFooter: Footer
-    }
-}
+  components: {
+    arcNavbar: Navbar,
+    arcFooter: Footer,
+    arcBanner: Banner
+  }
+};
 </script>
 
 <style>
 .navbar-container {
-    position: absolute;
-    top: 0;
-    margin-left: auto;
-    margin-right: auto;
-    left: 0;
-    right: 0;
+  position: absolute;
+  top: 0;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
 }
 .slide-enter-active {
-    animation: slide-in 200ms ease-out forwards;
+  animation: slide-in 200ms ease-out forwards;
 }
 .slide-leave-active {
-    animation: slide-out 200ms ease-out forwards;
+  animation: slide-out 200ms ease-out forwards;
 }
 
 @keyframes slide-in {
-    from {
-        opacity: 0;
-        transform: translateY(-30px);
-    }
-    to {
-        opacity: 1;
-        transform: translateY(0);
-    }
+  from {
+    opacity: 0;
+    transform: translateY(-30px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
 }
 @keyframes slide-out {
-    from {
-        opacity: 1;
-        transform: translateY(0);
-    }
-    to {
-        opacity: 0;
-        transform: translateY(-30px);
-    }
+  from {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateY(-30px);
+  }
 }
 
 .btn-primary {
-    background: linear-gradient(120deg, #36d1dc, #5b86e5);
-    border: none;
+  background: linear-gradient(120deg, #36d1dc, #5b86e5);
+  border: none;
 }
 
 .btn-primary:active {
-    background: linear-gradient(120deg, #26e1ec, #4b76d5) !important;
-    border: none;
+  background: linear-gradient(120deg, #26e1ec, #4b76d5) !important;
+  border: none;
 }
 </style>
---
+++ src/components/Banner/Banner.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="announcement-banner">
+    <h1>⚠️</h1>
+    <h1>
+      Hello 👋, there is a new version of this application at
+      <a
+        href="https://signmein.only"
+      >SignMeIn.online</a>. This app will shutdown on March 30, 2019.
+    </h1>
+  </section>
+</template>
+
+<style scoped>
+.announcement-banner {
+  /* position: absolute; */
+  width: 100%;
+  top: 0;
+  left: 0;
+  background: #f55d5d;
+  color: white;
+  padding: 16px;
+  text-align: center;
+}
+</style>
+
